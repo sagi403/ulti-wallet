69c9a10c1e75ee9000dea335b02954fdee9bd89d
Sagi <sagi403@gmail.com>
2022-09-19 14:58:47 +0300

    Backend setup

---
+++ backend/data/users.js
@@ -0,0 +1,14 @@
+const users = [
+  {
+    name: "John Doe",
+    email: "john@gmail.com",
+    password: "asdASD123!",
+  },
+  {
+    name: "Jane Doe",
+    email: "jane@gmail.com",
+    password: "asdASD123!",
+  },
+];
+
+export default users;
