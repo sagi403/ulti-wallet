3da8b6828d0ed85ddb2676b0122636def0c821d2
Sagi <sagi403@gmail.com>
2022-09-19 13:46:14 +0300

    Adding LoginScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import MainApp from "./components/MainApp";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const App = () => {
   return (
     <>
       {/* <MainApp /> */}
-      <HomeScreen />
+      {/* <HomeScreen /> */}
+      <LoginScreen />
     </>
   );
 };
