6eef68e92a0ddc9c1d2693c6de4a3cca6e7d73d9
Sagi <sagi403@gmail.com>
2022-09-21 13:50:02 +0300

    Data seeder script

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,13 +1,21 @@
+import bcrypt from "bcryptjs";
+
 const users = [
   {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
     name: "John Doe",
-    email: "john@gmail.com",
-    password: "asdASD123!",
+    email: "john@example.com",
+    password: bcrypt.hashSync("123456", 10),
   },
   {
     name: "Jane Doe",
-    email: "jane@gmail.com",
-    password: "asdASD123!",
+    email: "jane@example.com",
+    password: bcrypt.hashSync("123456", 10),
   },
 ];
 
