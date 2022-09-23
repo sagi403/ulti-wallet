fabe3c0d52cd1c2ca3f865bc3aea927c9940e789
Sagi <sagi403@gmail.com>
2022-09-23 14:25:40 +0300

    User login action

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -2,16 +2,21 @@ import { Form, Button, Container, Row, Col } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { login } from "../store/userSlice";
+import { useState } from "react";
+import axios from "axios";
 
 const LoginScreen = () => {
   const location = useLocation();
   const dispatch = useDispatch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const redirect = location.search ? location.search.split("=")[1] : "/";
 
-  const submitHandler = e => {
+  const submitHandler = async e => {
     e.preventDefault();
-    dispatch(login());
+
+    dispatch(login({ email, password }));
   };
 
   return (
@@ -20,12 +25,22 @@ const LoginScreen = () => {
       <Form onSubmit={submitHandler}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit">
--- frontend/src/store/userSlice.js
+++ frontend/src/store/userSlice.js
@@ -1,20 +1,53 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   userInfo: [],
-  loggedIn: false,
+  loading: true,
+  error: "",
 };
+
+export const login = createAsyncThunk("user/login", async (user, thunkApi) => {
+  try {
+    const { email, password } = user;
+    const config = { headers: { "Content-Type": "application/json" } };
+
+    const { data } = await axios.post(
+      "/api/users/login",
+      { email, password },
+      config
+    );
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+    return data;
+  } catch (error) {
+    thunkApi.rejectWithValue(error);
+  }
+});
 
 const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {
-    login: state => {
-      state.loggedIn = true;
+  // reducers: {
+  //   login: state => {
+  //     state.loggedIn = true;
+  //   },
+  // },
+  extraReducers: {
+    [login.pending]: state => {
+      state.loading = true;
+    },
+    [login.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.userInfo = action.payload;
+    },
+    [login.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
     },
   },
 });
 
-export const { login } = userSlice.actions;
+// export const { login } = userSlice.actions;
 
 export default userSlice.reducer;
