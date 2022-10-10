3a3145a1e5ac806b6ff432c9f833fbc656686aa5
Sagi <sagi403@gmail.com>
2022-10-10 16:38:39 +0300

    Adding coins id route

--- backend/controllers/coinControllers.js
+++ backend/controllers/coinControllers.js
@@ -18,4 +18,31 @@ const getUserCoins = asyncHandler(async (req, res) => {
   res.json(coins);
 });
 
-export { getUserCoins };
+// @desc    Get coins id
+// @route   GET /api/coins/coinsId
+// @access  Private
+const getCoinsId = asyncHandler(async (req, res) => {
+  const { rows: coins } = await pool.query(
+    "SELECT coins.id FROM addresses INNER JOIN coins ON addresses.coin_symbol = coins.symbol WHERE user_id = $1",
+    [req.user.id]
+  );
+
+  if (coins.length === 0) {
+    res.status(404);
+    throw new Error("Coin not found");
+  }
+
+  const coinsId = [];
+
+  for (let coin of coins) {
+    const { id } = coin;
+
+    if (!coinsId.includes(id)) {
+      coinsId.push(id);
+    }
+  }
+
+  res.json(coinsId);
+});
+
+export { getUserCoins, getCoinsId };
--- backend/routes/coinRoutes.js
+++ backend/routes/coinRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getUserCoins } from "../controllers/coinControllers.js";
+import { getCoinsId, getUserCoins } from "../controllers/coinControllers.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").get(protect, getUserCoins);
+router.route("/coinsId").get(protect, getCoinsId);
 
 export default router;
