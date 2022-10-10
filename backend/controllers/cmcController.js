ab47ac18bd8a80cfdf86d0be0eaa7fd3af5ddc8b
Sagi <sagi403@gmail.com>
2022-10-10 17:12:30 +0300

    Fetching coins data from cmc api

--- backend/controllers/cmcController.js
+++ backend/controllers/cmcController.js
@@ -1,12 +1,35 @@
 import asyncHandler from "express-async-handler";
+import axios from "axios";
 import pool from "../db/index.js";
 
 // @desc    Get CMC coins data
 // @route   GET /api/cmc
 // @access  Private
 const getCmcCoins = asyncHandler(async (req, res) => {
-  const { coins_id } = req.body;
-  res.json(coins);
+  const { coinsId } = req.body;
+
+  const config = {
+    headers: {
+      "X-CMC_PRO_API_KEY": process.env.CMC_API_KEY,
+    },
+  };
+
+  const {
+    data: { data },
+  } = await axios.get(
+    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
+    config
+  );
+
+  const coinsInfo = [];
+
+  for (let coin of data) {
+    if (coinsId.includes(coin.id)) {
+      coinsInfo.push(coin);
+    }
+  }
+
+  res.json(coinsInfo);
 });
 
 export { getCmcCoins };
