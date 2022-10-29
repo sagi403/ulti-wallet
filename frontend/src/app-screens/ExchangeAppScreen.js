c80b015c3a303f67d7ded9f3e1c011a74a29a8aa
Sagi <sagi403@gmail.com>
2022-10-29 15:54:43 +0300

    Designing base display

--- frontend/src/app-screens/ExchangeAppScreen.js
+++ frontend/src/app-screens/ExchangeAppScreen.js
@@ -1,5 +1,52 @@
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+
 const ExchangeAppScreen = () => {
-  return <div>ExchangeAppScreen</div>;
+  return (
+    <Container fluid className="portfolio">
+      <Container className="coin_cards">
+        <div className="coin_card_upper">
+          <Row className="m-0">
+            <Col className="d-flex justify-content-start">
+              <p>Balance:0.4214</p>
+            </Col>
+          </Row>
+          <Row className="m-0">
+            <Col className="d-flex justify-content-start">
+              <h2>BTC</h2>
+            </Col>
+            <Col className="d-flex justify-content-end">
+              <h2>0.36851</h2>
+            </Col>
+          </Row>
+          <Row className="m-0">
+            <Col className="d-flex justify-content-end">
+              <p>Balance:0.4214</p>
+            </Col>
+          </Row>
+        </div>
+        <div className="coin_card_lower">
+          <Row className="m-0">
+            <Col className="d-flex justify-content-start">
+              <p>Balance:0.4214</p>
+            </Col>
+          </Row>
+          <Row className="m-0">
+            <Col className="d-flex justify-content-start">
+              <h2>BTC</h2>
+            </Col>
+            <Col className="d-flex justify-content-end">
+              <h2>0.36851</h2>
+            </Col>
+          </Row>
+          <Row className="m-0">
+            <Col className="d-flex justify-content-end">
+              <p>Balance:0.4214</p>
+            </Col>
+          </Row>
+        </div>
+      </Container>
+    </Container>
+  );
 };
 
 export default ExchangeAppScreen;
