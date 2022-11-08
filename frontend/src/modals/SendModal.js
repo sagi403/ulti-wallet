9a6a5827d2916b6e9604d2479fa68639af44d60e
Sagi <sagi403@gmail.com>
2022-11-08 12:48:29 +0200

    Adding design to the send modal

--- frontend/src/modals/SendModal.js
+++ frontend/src/modals/SendModal.js
@@ -7,6 +7,7 @@ import validate from "../validation/validate";
 const SendModal = ({ show, onHide, coinInfo }) => {
   const [address, setAddress] = useState("");
   const [addressValidateMsg, setAddressValidateMsg] = useState(null);
+  const [amount, setAmount] = useState("");
 
   useEffect(() => {
     setAddress("");
@@ -34,27 +35,59 @@ const SendModal = ({ show, onHide, coinInfo }) => {
       <Modal.Header closeButton className="coin_exchange_from_table" />
       <Modal.Body className="coin_exchange_from_table">
         <div className="successful_swap_body">
-          <div className="polygon position-relative mb-5">
+          <div className="mb-5">
             <img src={coinInfo.logo} alt={coinInfo.name} />
           </div>
-          <Form.Control
-            type="text"
-            value={address}
-            onChange={e => setAddress(e.target.value)}
-            placeholder={`Send to ${coinInfo.name} address...`}
-            className="send_input"
-            isInvalid={addressValidateMsg}
-            onBlur={handleAddressBlur}
-          />
-          <Form.Control.Feedback type="invalid">
-            {addressValidateMsg}
-          </Form.Control.Feedback>
+          <div className="inputs_container">
+            <div className="my-4">
+              <Form.Control
+                type="text"
+                value={address}
+                onChange={e => setAddress(e.target.value)}
+                placeholder={`Send to ${coinInfo.name} address...`}
+                className="send_input"
+                isInvalid={addressValidateMsg}
+                onBlur={handleAddressBlur}
+              />
+              <Form.Control.Feedback type="invalid">
+                {addressValidateMsg}
+              </Form.Control.Feedback>
+            </div>
+            <div className="coin_amount_container d-flex align-items-center">
+              <Form.Control
+                type="number"
+                placeholder="0.00"
+                className="sent_amount d-inline float-start"
+                style={{ color: coinInfo.color }}
+                value={amount}
+                onChange={e => setAmount(+e.target.value || "")}
+                // onBlur={() => handleSetCoinAmount(coin)}
+              />
+              <div className="d-flex ms-auto">
+                <p className="send_amount_auto_btn">ALL</p>
+                <p className="send_amount_auto_btn">HALF</p>
+                <span className="ms-3 fs-5" style={{ color: coinInfo.color }}>
+                  {coinInfo.symbol}
+                </span>
+              </div>
+            </div>
+            <hr
+              className="send_hr m-0"
+              style={{ background: coinInfo.color }}
+            />
+            <div className="coin_amount_container d-flex align-items-center">
+              <span className="usd_send_value">0.00</span>
+              <div className="d-flex ms-auto">
+                <span className="ms-3 fs-5 chart_total_value">USD</span>
+              </div>
+            </div>
+          </div>
         </div>
       </Modal.Body>
       <Modal.Footer className="coin_exchange_from_table">
         <Button
           onClick={onHide}
-          className="close_modal"
+          className="close_modal mx-auto my-4 w-25"
           style={{ borderColor: coinInfo.color }}
         >
           Send
